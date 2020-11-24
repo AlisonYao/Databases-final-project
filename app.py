36905c23983040ed306ed79fa98862480751ab87
Alison Yao <yy2564@nyu.edu>
2020-11-24 12:30:47 +0800

    finish customer page design

--- app.py
+++ app.py
@@ -194,7 +194,27 @@ def cushome():
     cursor.execute(query.format(email))
     data1 = cursor.fetchall() 
     cursor.close()
-    return render_template('cushome.html', email=email, posts=data1)
+    return render_template('cushome.html', email=email, view_my_flights=data1)
+
+@app.route('/cusSearchPurchase')
+def cusSearchPurchase():
+	email = session['email'] 
+	return render_template('cusSearchPurchase.html', email=email)
+
+@app.route('/cusSpending', methods=['POST', 'GET'])
+def cusSpending():
+	email = session['email']
+	# cursor = conn.cursor()
+	# duration = request.form.get("duration")
+	# if duration is None:
+	# 	duration = "30"
+
+	# query = 'select sum(ticket_price * 0.1), avg(ticket_price * 0.1), count(ticket_price * 0.1) from agent_commission where email = \'{}\' and (purchase_date between DATE_ADD(NOW(), INTERVAL -\'{}\' DAY) and NOW())'
+	# cursor.execute(query.format(email, duration))
+	# commission_data = cursor.fetchone()
+	# total_com, avg_com, count_ticket = commission_data
+	# cursor.close()
+	return render_template('cusSpending.html', email=email)
 
 @app.route('/cusSearchFlight', methods=['GET', 'POST'])
 def cusSearchFlight():
--- temp.py
+++ temp.py
@@ -1,43 +1,14 @@
-@app.route('/agentSearchFlight', methods=['GET', 'POST'])
-def agentSearchFlight():
+@app.route('/cusSpending', methods=['POST', 'GET'])
+def cusSpending():
 	email = session['email']
-	departure_city = request.form['departure_city']
-	departure_airport = request.form['departure_airport']
-	arrival_city = request.form['arrival_city']
-	arrival_airport = request.form['arrival_airport']
-	departure_date = request.form['departure_date']
-	arrival_date = request.form['arrival_date']
+	# cursor = conn.cursor()
+	# duration = request.form.get("duration")
+	# if duration is None:
+	# 	duration = "30"
 
-	# validate booking agent email
-	cursor = conn.cursor()
-	query = " select booking_agent_id from booking_agent where email = \'{}\'"
-	cursor.execute(query.format(email))
-	agent_data = cursor.fetchone() # tuple (98765,)
-	booking_agent_id = agent_data[0]
-	cursor.close()
-
-	if not (agent_data):
-		agent_id_error = 'You are not a booking agent'
-		return render_template('agentSearchPurchase.html', error1=agent_id_error)
-
-	# booking agent email validated
-	cursor = conn.cursor()
-	query = "SELECT airplane_id, flight_num, D.airport_city, departure_airport, A.airport_city, arrival_airport, departure_time, arrival_time, status, price \
-				FROM airport as D, flight, airport AS A \
-				WHERE D.airport_city = if (\'{}\' = '',D.airport_city, \'{}\') and \
-				D.airport_name = departure_airport and \
-				departure_airport = if (\'{}\' = '', departure_airport, \'{}\') and \
-				A.airport_city = if (\'{}\' = '', A.airport_city, \'{}\')and \
-				A.airport_name = arrival_airport and \
-				arrival_airport =  if (\'{}\' = '', arrival_airport, \'{}\')and \
-				date(departure_time) = if (\'{}\' = '', date(departure_time), \'{}\')and \
-				date(arrival_time) =  if (\'{}\' = '', date(arrival_time), \'{}\')"
-	cursor.execute(query.format(departure_city, departure_city,departure_airport,departure_airport, arrival_city, arrival_city, arrival_airport, arrival_airport, departure_date, departure_date, arrival_date, arrival_date))
-	data = cursor.fetchall()
-	
-	error = None
-	if (data): # has data
-		return render_template('agentSearchPurchase.html', email=email, upcoming_flights=data)
-	else: # does not have data
-		error = 'Sorry ... Cannot find this flight!'
-		return render_template('agentSearchPurchase.html', email=email, error1=error)
+	# query = 'select sum(ticket_price * 0.1), avg(ticket_price * 0.1), count(ticket_price * 0.1) from agent_commission where email = \'{}\' and (purchase_date between DATE_ADD(NOW(), INTERVAL -\'{}\' DAY) and NOW())'
+	# cursor.execute(query.format(email, duration))
+	# commission_data = cursor.fetchone()
+	# total_com, avg_com, count_ticket = commission_data
+	# cursor.close()
+	return render_template('cusSpending.html', email=email)
